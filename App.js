88e60ce35870df3e63da5fa40014e1b7889939bf
MobikTech <62842262+MobikTech@users.noreply.github.com>
2021-12-20 16:37:12 +0200

    init

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {globalStyle} from "./styles/style";
+import AppLoading from 'expo-app-loading';
+import {useFonts} from "expo-font";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let [fontsLoaded] = useFonts({
+        'pp-light': require('./assets/fonts/Poppins-ExtraLight.ttf'),
+        'pp-medium': require('./assets/fonts/Poppins-Medium.ttf'),
+        'pp-bold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    } else {
+        return (
+              <View style={styles.mainContainer}>
+                <Text style={globalStyle.text}>Havchik</Text>
+              </View>
+          );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    mainContainer:{
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'green'
+    }
 });
+
---
+++ styles/style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native";
+
+export const globalStyle = StyleSheet.create({
+    header: {
+
+    },
+    text: {
+        backgroundColor: 'red',
+        fontFamily: 'pp-bold',
+        // fontFamily: 'pp-medium',
+        // fontFamily: 'pp-light',
+        fontSize: 30,
+    },
+
+})
